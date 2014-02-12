abcd93092079872a2a399e533156d9d4217ed3e5
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-02-12 13:41:19 +0100

    tiny should go, so I comment its tests

--- plone/app/widgets/tests/test_at.py
+++ plone/app/widgets/tests/test_at.py
@@ -399,12 +399,12 @@ class TinyMCEWidgetTests(unittest.TestCase):
         self.assertEqual(base_args['name'], 'fieldname')
         self.assertEqual(base_args['value'], 'fieldvalue')
         self.assertEqual(base_args['pattern'], 'tinymce')
-        self.assertEqual(base_args['pattern_options']['pattern_options']['prependToUrl'],
-                         'resolveuid/')
-        self.assertEqual(base_args['pattern_options']['pattern_options']['prependToUrl'],
-                         'resolveuid/')
-        self.assertEqual(base_args['pattern_options']['pattern_options']['anchorSelector'],
-                         self.portal.portal_tinymce.anchor_selector)
+        #self.assertEqual(base_args['pattern_options']['pattern_options']['prependToUrl'],
+                         #'resolveuid/')
+        #self.assertEqual(base_args['pattern_options']['pattern_options']['prependToUrl'],
+                         #'resolveuid/')
+        #self.assertEqual(base_args['pattern_options']['pattern_options']['anchorSelector'],
+                         #self.portal.portal_tinymce.anchor_selector)
 
 
 class ArchetypesVocabularyPermissionTests(unittest.TestCase):
